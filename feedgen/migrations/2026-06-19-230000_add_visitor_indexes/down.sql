cb4476e8775e47d6d4840e86d2c54567a800c9d4
Ripperoni <email@ripperoni.com>
2026-06-19 23:03:23 -0700

    feat: add indexes on visitor table and fix postgres healthcheck in docker-compose

---
+++ feedgen/migrations/2026-06-19-230000_add_visitor_indexes/down.sql
@@ -0,0 +1,2 @@
+DROP INDEX IF EXISTS visitor_did_idx;
+DROP INDEX IF EXISTS visitor_visited_at_idx;
---
+++ feedgen/migrations/2026-06-19-230000_add_visitor_indexes/up.sql
@@ -0,0 +1,2 @@
+CREATE INDEX IF NOT EXISTS visitor_did_idx ON visitor (did);
+CREATE INDEX IF NOT EXISTS visitor_visited_at_idx ON visitor (visited_at);
